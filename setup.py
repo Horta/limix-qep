71b0f032fac22cfb71a39184bc81d2075553fd52
Danilo Horta <danilo.horta@gmail.com>
2016-03-11 15:06:41 +0000

    removed limix tool dependency

--- setup.py
+++ setup.py
@@ -112,7 +112,7 @@ def setup_package():
 
     write_version()
 
-    install_requires = ['hcache', 'limix_math', 'limix_tool']
+    install_requires = ['hcache', 'limix_math']
     setup_requires = []
 
     metadata = dict(
