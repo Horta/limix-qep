d3d037bb36907a13e25f15c9015df372a4202e7d
Danilo Horta <danilo.horta@gmail.com>
2016-07-26 22:11:08 +0100

    over

--- limix_qep/ep/overdispersion.py
+++ limix_qep/ep/overdispersion.py
@@ -3,34 +3,16 @@ from __future__ import division
 
 import logging
 
-import numpy as np
-
-from limix_math.linalg import sum2diag
-from limix_math.dist.beta import isf as bisf
-
-from hcache import cached
-from .dists import Joint
-
-from .config import HYPERPARAM_EPS
-from .config import R_EPS
-
 from scipy.optimize import minimize_scalar
 
-from limix_qep.special.nbinom_moms import moments_array3, init
+from hcache import cached
 
-from numpy import var as variance
-from numpy import exp
-from numpy import log
+from limix_math.linalg import sum2diag
 
+from .config import HYPERPARAM_EPS
+from .dists import Joint
 from .util import normal_bracket
-
 from .base import EP
-
-_NALPHAS0 = 100
-_ALPHAS0 = bisf(0.55, 2., 1.-
-                np.linspace(0, 1, _NALPHAS0+1, endpoint=False)[1:])
-_NALPHAS1 = 30
-_ALPHAS1_EPS = 1e-3
 
 
 # K = Q (v S + e I) Q.T
@@ -62,12 +44,14 @@ class OverdispersionEP(EP):
     @property
     def noise_ratio(self):
         e = self.environmental_variance
-        return e / (e + 1)
+        i = self.instrumental_variance
+        return e / (e + i)
 
     @noise_ratio.setter
     def noise_ratio(self, value):
         value = min(value, 1-1e-7)
-        self.environmental_variance = value / (1 - value)
+        i = self.instrumental_variance
+        self.environmental_variance = i * value / (1 - value)
 
     @property
     def environmental_variance(self):
@@ -91,9 +75,9 @@ class OverdispersionEP(EP):
 
     def _noise_ratio_cost(self, r):
         print("  - Evaluating for ratio: %.5f." % r)
-        self.e = r / (1 - r)
+        self.environmental_variance = r / (1 - r)
         h2 = self.heritability
-        self.var = h2 * self.e / (1 - h2)
+        self.genetic_variance = h2 * self.environmental_variance / (1 - h2)
         self._optimize_beta()
         c = -self.lml()
         print("  - Cost: %.5f" % c)
@@ -103,16 +87,15 @@ class OverdispersionEP(EP):
         # self._logger.debug("Evaluating for h2: %e.", h2)
         print("- Evaluating for h2: %.5f." % h2)
         var = self.h2tovar(h2)
-        self.var = var
+        self.genetic_variance = var
 
-        r = self.e / (self.e + 1)
-        p = min(abs(self._previous_h2 - h2)*100, 1)
-        bracket = normal_bracket(r, p)
+        r = self.environmental_variance / (self.environmental_variance + 1)
+        bracket = normal_bracket(r, 1.)
         print("    Ratio bracket: %s." % str(bracket))
         try:
             res = minimize_scalar(self._noise_ratio_cost,
                                 bracket=bracket,
-                                tol=R_EPS, method='Brent')
+                                tol=HYPERPARAM_EPS, method='Brent')
         except ValueError:
             fa = self._noise_ratio_cost(bracket[0])
             fc = self._noise_ratio_cost(bracket[2])
@@ -123,12 +106,10 @@ class OverdispersionEP(EP):
         else:
             r = res.x
 
-        self._previous_h2 = h2
-        self.e = r / (1 - r)
+        self.environmental_variance = r / (1 - r)
         return -self.lml()
 
     def optimize(self):
-        self._previous_h2 = self.heritability
         super(OverdispersionEP, self).optimize()
 
     ############################################################################
@@ -145,19 +126,19 @@ class OverdispersionEP(EP):
     def _A1(self):
         """:math:`(e \\mathrm I + \\tilde{\\mathrm T}^{-1})^{-1}`"""
         ttau = self._sites.tau
-        return ttau / (self.e * ttau + 1)
+        return ttau / (self.environmental_variance * ttau + 1)
 
     @cached
     def _A2(self):
         """:math:`\\tilde{\\mathrm T}^{-1} \\mathrm A_1`"""
         ttau = self._sites.tau
-        return 1 / (self.e * ttau + 1)
+        return 1 / (self.environmental_variance * ttau + 1)
 
     @cached
     def K(self):
         """:math:`K = (v Q S Q.T + e I)`"""
-        return sum2diag(self.var * self._QSQt(), self.e)
+        return sum2diag(self.genetic_variance * self._QSQt(), self.environmental_variance)
 
     @cached
     def diagK(self):
-        return self.var * self._diagQSQt() + self.e
+        return self.genetic_variance * self._diagQSQt() + self.environmental_variance
