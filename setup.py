01e7ea9b582f7c8c2630037fe5d346a5bfde088b
Danilo Horta <danilo.horta@gmail.com>
2016-08-11 10:20:35 +0100

    requires limix math 0.1.9

--- setup.py
+++ setup.py
@@ -117,7 +117,7 @@ def setup_package():
 
     write_version()
 
-    install_requires = ['hcache', 'limix_math>=0.1.8', 'limix_tool',
+    install_requires = ['hcache', 'limix_math>=0.1.9', 'limix_tool',
                         'limix_util', 'lim']
     setup_requires = ['pytest-runner']
     tests_require = ['pytest']
