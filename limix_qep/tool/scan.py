4633541f8a778465b3380023eda92c3085a65f36
Danilo Horta <danilo.horta@gmail.com>
2016-08-11 14:15:26 +0100

    adjusting this fixed_ep

--- limix_qep/tool/scan.py
+++ limix_qep/tool/scan.py
@@ -5,19 +5,25 @@ from numpy import asarray
 import numpy as np
 from numpy import dot
 from limix_qep import Bernoulli, Binomial
-from limix_math.linalg import economic_QS
+from limix_math.linalg import qs_decomposition
+from limix_math.linalg import _QS_from_K_split
 from .util import gower_kinship_normalization
 import scipy.stats as st
 from time import time
+from limix_qep.ep import BernoulliEP
+from limix_qep.ep import BinomialEP
+
 
 def _get_offset_covariate(covariate, n):
     if covariate is None:
         covariate = np.ones((n, 1))
     return covariate
 
+
 class LRT(object):
-    def __init__(self, y, QS, outcome_type=Bernoulli(), full=False,
-                    covariate=None, null_model_only=False):
+
+    def __init__(self, y, Q0, Q1, S0, outcome_type=Bernoulli(), full=False,
+                 covariate=None, null_model_only=False):
 
         self._logger = logging.getLogger(__name__)
 
@@ -29,35 +35,36 @@ class LRT(object):
 
         self._full = full
         self._y = y
-        self._Q = QS[0]
-        self._S = QS[1]
+        self._Q0 = Q0
+        self._Q1 = Q1
+        self._S0 = S0
         self._covariate = _get_offset_covariate(covariate, y.shape[0])
         self._outcome_type = outcome_type
         self._null_model_ready = False
         self._alt_model_ready = False
 
-        self._varg = np.nan
-        self._varo = np.nan
-        self._vare = np.nan
+        self._genetic_variance = None
+        self._instrumental_variance = None
+        self._environmental_variance = None
         self._pvals = None
         self._lrs = None
         self._ep = None
-        self._betas  = None
+        self._betas = None
         self._lml_null = np.nan
         self._X = None
         self._null_model_only = null_model_only
 
     @property
-    def varg(self):
-        return self._varg
+    def genetic_variance(self):
+        return self._genetic_variance
 
     @property
-    def varo(self):
-        return self._varo
+    def instrumental_variance(self):
+        return self._instrumental_variance
 
     @property
-    def vare(self):
-        return self._vare
+    def environmental_variance(self):
+        return self._environmental_variance
 
     @property
     def candidate_markers(self):
@@ -123,49 +130,26 @@ class LRT(object):
         self._logger.info('Null model computation has started.')
 
         y = self._y
-        Q = self._Q
-        S = self._S
+        Q0, Q1 = self._Q0, self._Q1
+        S0 = self._S0
         covariate = self._covariate
         outcome_type = self._outcome_type
 
-        before = time()
-        ep = EP(y, covariate, Q, S, outcome_type)
-        # import pylab as plt
-        # lmls_ = []
-        # h2s_ = []
-        # for h2i in np.linspace(1e-4, 1 - 1e-4, 1000):
-        #     h2s_.append(h2i)
-        #     sigg2 = ep.h2tosigg2(h2i)
-        #     ep.sigg2 = sigg2
-        #     lmls_.append(ep.lml())
-        # plt.plot(h2s_, lmls_)
-        # mindx = np.argmax(lmls_)
-        # plt.axvline(h2s_[mindx])
-        # print(h2s_[mindx])
-        # plt.savefig('lml.png')
-        # plt.close()
-        total = time() - before
-        # print("EP init: %.5f" % total)
-        before = time()
+        import ipdb
+        ipdb.set_trace()
+        if isinstance(outcome_type, Binomial):
+            ep = BinomialEP(y, outcome_type.ntrials, covariate, Q0, Q1, S0)
+        elif isinstance(outcome_type, Bernoulli):
+            ep = BernoulliEP(y, covariate, Q0, Q1, S0)
+
         ep.optimize()
-        total = time() - before
-        # print("ep.optimize(): %.5f" % total)
 
-        lml_null = ep.lml()
-
-        self._lml_null = lml_null
+        self._lml_null = ep.lml()
         self._ep = ep
-        # self._ep_sites = ep.get_site_likelihoods()
 
-        varg = ep.sigg2
-        varo = ep.sigg2 * ep.delta
-        vare = 1.
-        vart = varg + varo + vare
-
-        self._varg = varg / vart
-        self._varo = varo / vart
-        self._vare = vare / vart
-
+        self._genetic_variance = ep.genetic_variance
+        self._instrumental_variance = ep.instrumental_variance
+        self._environmental_variance = ep.environmental_variance
         self._null_model_ready = True
 
     def _compute_alt_models_full(self):
@@ -178,7 +162,7 @@ class LRT(object):
 
         lml_alts = []
         for i in range(X.shape[1]):
-            ep.M = np.hstack( (covariate, X[:,i][:,np.newaxis]) )
+            ep.M = np.hstack((covariate, X[:, i][:, np.newaxis]))
             assert False, 'fix me'
             # ep.optimize(only_step2=True)
             lml_alts.append(ep.lml())
@@ -199,16 +183,16 @@ class LRT(object):
         lml_alt = []
 
         p = covariate.shape[1]
-        acov = np.hstack( (covariate, X) )
+        acov = np.hstack((covariate, X))
         self._logger.debug('Finding optimal betas.')
         if p == 1:
             betas = fep.optimal_betas(acov, 1)
         else:
             betas = fep.optimal_betas_general(acov, p)
 
-        ms = dot(covariate, betas[:p,:]) + X * betas[p,:]
+        ms = dot(covariate, betas[:p, :]) + X * betas[p, :]
         lml_alt = fep.lmls(ms)
-        self._betas = betas[p,:]
+        self._betas = betas[p, :]
         return lml_alt
 
     @property
@@ -236,7 +220,7 @@ class LRT(object):
         S = QS[1]
         if np.any(y != self._y):
             return False
-        if Q[0,0] != self._Q[0,0] or S[0] != self._S[0]:
+        if Q[0, 0] != self._Q[0, 0] or S[0] != self._S[0]:
             return False
         if np.any(S != self._S):
             return False
@@ -249,22 +233,28 @@ class LRT(object):
             return False
         return True
 
-def _create_LRT(y, QS, covariate, outcome_type, null_model_only):
-    do_create = False
 
-    if _create_LRT.cache is None:
-        do_create = True
-    else:
-        do_create = not _create_LRT.cache.is_valid(y, QS, covariate,
-                                                   outcome_type)
+# def _create_LRT(y, QS, covariate, outcome_type, null_model_only):
+#     do_create = False
+#
+#     if _create_LRT.cache is None:
+#         do_create = True
+#     else:
+#         do_create = not _create_LRT.cache.is_valid(y, QS, covariate,
+#                                                    outcome_type)
+#
+#     if do_create:
+#         _create_LRT.cache = LRT(y, QS, covariate=covariate,
+#                                 outcome_type=outcome_type,
+#                                 null_model_only=null_model_only)
+#
+#     return _create_LRT.cache
+# _create_LRT.cache = None
 
-    if do_create:
-        _create_LRT.cache = LRT(y, QS, covariate=covariate,
-                                    outcome_type=outcome_type,
-                                    null_model_only=null_model_only)
+def _create_LRT(y, Q0, Q1, S0, covariate, outcome_type, null_model_only):
+    return LRT(y, Q0, Q1, S0, covariate=covariate, outcome_type=outcome_type,
+               null_model_only=null_model_only)
 
-    return _create_LRT.cache
-_create_LRT.cache = None
 
 def scan(y, X, G=None, K=None, QS=None, covariate=None,
          outcome_type=None, null_model_only=False):
@@ -323,7 +313,7 @@ def scan(y, X, G=None, K=None, QS=None, covariate=None,
         G = G - np.mean(G, 0)
         s = np.std(G, 0)
         ok = s > 0.
-        G[:,ok] /= s[ok]
+        G[:, ok] /= s[ok]
         G /= np.sqrt(G.shape[1])
         info['G'] = G
         total = time() - before
@@ -333,24 +323,32 @@ def scan(y, X, G=None, K=None, QS=None, covariate=None,
     if G is None and K is None and QS is None:
         raise Exception('G, K, and QS cannot be all None.')
 
+    import ipdb
+    ipdb.set_trace()
     if QS is None:
         logger.info('Computing the economic eigen decomposition.')
-        QS = economic_QS((G, K), 'GK')
+        if K is None:
+            QS = qs_decomposition(G)
+        else:
+            QS = _QS_from_K_split(K)
+
+        Q0, Q1 = QS[0]
+        S0 = QS[1][0]
     else:
-        Q = QS[0]
-        S = QS[1]
-        S /= S.mean()
-        QS = (Q, S)
+        Q0 = QS[0]
+        S0 = QS[1]
+        S0 /= S0.mean()
 
     logger.info('Genetic marker candidates normalization.')
     X = X - np.mean(X, 0)
     s = np.std(X, 0)
     ok = s > 0.
-    X[:,ok] /= s[ok]
+    X[:, ok] /= s[ok]
     X /= np.sqrt(X.shape[1])
     info['X'] = X
 
-    lrt = _create_LRT(y, QS, covariate, outcome_type, null_model_only=null_model_only)
+    lrt = _create_LRT(y, Q0, Q1, S0, covariate, outcome_type,
+                      null_model_only=null_model_only)
     lrt.candidate_markers = X
     before = time()
     info['lrs'] = lrt.lrs()
@@ -361,74 +359,95 @@ def scan(y, X, G=None, K=None, QS=None, covariate=None,
 
     print("Total time spend:            !!__%.5f__!!" % (time() - before_all))
 
-    # print("nchol:%d, ncholm:%d, nmult: %d" % (lrt._ep.nchol(), lrt._ep.ncholm(),
-        #   lrt._ep.nmult()))
+    return return_
 
-    # print("beta %d %.5f %.5f" % (lrt._ep._calls['beta'], lrt._ep._time_elapsed['beta']/lrt._ep._calls['beta'], lrt._ep._time_elapsed['beta']))
-    # print("sigg2 %d %.5f %.5f" % (lrt._ep._calls['sigg2'], lrt._ep._time_elapsed['sigg2']/lrt._ep._calls['sigg2'], lrt._ep._time_elapsed['sigg2']))
-    # print("sigg2-beta %.5f %.5f" % ((lrt._ep._time_elapsed['sigg2']-lrt._ep._calls['beta'])/lrt._ep._calls['sigg2'], (lrt._ep._time_elapsed['sigg2']-lrt._ep._calls['beta'])))
-    #
-    # print("eploop %d %.5f %.5f" % (lrt._ep._calls['eploop'], lrt._ep._time_elapsed['eploop']/lrt._ep._calls['eploop'], lrt._ep._time_elapsed['eploop']))
-    #
-    # print("before_eploop %d %.5f %.5f" % (lrt._ep._calls['before_eploop'], lrt._ep._time_elapsed['before_eploop']/lrt._ep._calls['before_eploop'], lrt._ep._time_elapsed['before_eploop']))
-    #
-    # print("eploop_init %d %.5f %.5f" % (lrt._ep._calls['eploop_init'], lrt._ep._time_elapsed['eploop_init']/lrt._ep._calls['eploop_init'], lrt._ep._time_elapsed['eploop_init']))
-    #
-    # print("L1 %d %.5f %.5f" % (lrt._ep._calls['L1'], lrt._ep._time_elapsed['L1']/lrt._ep._calls['L1'], lrt._ep._time_elapsed['L1']))
-    #
-    # print("_QtA1Q %d %.5f %.5f" % (lrt._ep._calls['_QtA1Q'], lrt._ep._time_elapsed['_QtA1Q']/lrt._ep._calls['_QtA1Q'], lrt._ep._time_elapsed['_QtA1Q']))
-    #
-    # print("_B1 %d %.5f %.5f" % (lrt._ep._calls['_B1'], lrt._ep._time_elapsed['_B1']/lrt._ep._calls['_B1'], lrt._ep._time_elapsed['_B1']))
-    #
-    # print("tilted_params_bernoulli %d %.5f %.5f" % (lrt._ep._calls['tilted_params_bernoulli'], lrt._ep._time_elapsed['tilted_params_bernoulli']/lrt._ep._calls['tilted_params_bernoulli'], lrt._ep._time_elapsed['tilted_params_bernoulli']))
-    #
-    #
-    # print("JOINT initialize %d %.5f %.5f" % (lrt._ep._joint._calls['initialize'], lrt._ep._joint._time_elapsed['initialize']/lrt._ep._joint._calls['initialize'], lrt._ep._joint._time_elapsed['initialize']))
-    # print("JOINT update %d %.5f %.5f" % (lrt._ep._joint._calls['update'], lrt._ep._joint._time_elapsed['update']/lrt._ep._joint._calls['update'], lrt._ep._joint._time_elapsed['update']))
-    #
-    # print("JOINT cho_solve %d %.5f %.5f" % (lrt._ep._joint._calls['cho_solve'], lrt._ep._joint._time_elapsed['cho_solve']/lrt._ep._joint._calls['cho_solve'], lrt._ep._joint._time_elapsed['cho_solve']))
-    # print("JOINT mult %d %.5f %.5f" % (lrt._ep._joint._calls['mult'], lrt._ep._joint._time_elapsed['mult']/lrt._ep._joint._calls['mult'], lrt._ep._joint._time_elapsed['mult']))
-    # print("JOINT ddot1 %d %.5f %.5f" % (lrt._ep._joint._calls['ddot1'], lrt._ep._joint._time_elapsed['ddot1']/lrt._ep._joint._calls['ddot1'], lrt._ep._joint._time_elapsed['ddot1']))
-    # print("JOINT ddot2 %d %.5f %.5f" % (lrt._ep._joint._calls['ddot2'], lrt._ep._joint._time_elapsed['ddot2']/lrt._ep._joint._calls['ddot2'], lrt._ep._joint._time_elapsed['ddot2']))
-    # print("JOINT ddot3 %d %.5f %.5f" % (lrt._ep._joint._calls['ddot3'], lrt._ep._joint._time_elapsed['ddot3']/lrt._ep._joint._calls['ddot3'], lrt._ep._joint._time_elapsed['ddot3']))
-    # print("JOINT dotvec %d %.5f %.5f" % (lrt._ep._joint._calls['dotvec'], lrt._ep._joint._time_elapsed['dotvec']/lrt._ep._joint._calls['dotvec'], lrt._ep._joint._time_elapsed['dotvec']))
-    #
-    # print('Heritability: %.8f' % lrt._ep.h2())
-    #
-    # import pylab as plt
-    # N = 7
-    #
-    # outer_iter = 1
-    # for k in lrt._ep._ttau:
-    #     plt.figure()
-    #     i = 0
-    #     for ttau in lrt._ep._ttau[k]:
-    #         plt.plot(ttau)
-    #         i += 1
-    #         if i == N:
-    #             break
-    #
-    #     plt.axhline(1./(k+1), color='black')
-    #     plt.axhline(np.mean(lrt._ep._ttau[k][-1]), color='white')
-    #     plt.ylim([0, 1.0])
-    #     plt.savefig('%d/iter/%02d_%f_%f.png' % (N, outer_iter, k, lrt._ep._betas[outer_iter-1]))
-    #     plt.savefig('%d/sig/%f_%02d.png' % (N, k, outer_iter))
-    #     plt.close()
-    #     outer_iter += 1
-    #
-    #
-    # outer_iter = 1
-    # for k in lrt._ep._ttau:
-    #     plt.figure()
-    #     i = 0
-    #     for ttau in lrt._ep._ttau[k]:
-    #         plt.plot(np.argsort(ttau))
-    #         i += 1
-    #         if i == N:
-    #             break
-    #
-    #     plt.savefig('%d/rank/%02d_%f.png' % (N, outer_iter, k))
-    #     plt.close()
-    #     outer_iter += 1
 
+def scan_binomial(nsuccesses, ntrials, X, G=None, K=None, covariate=None):
+    """Perform association scan between genetic markers and phenotype.
+
+    Matrix `X` shall contain the genetic markers (e.g., number of minor alleles)
+    with rows and columsn representing samples and genetic markers,
+    respectively.
+
+    The user must specifiy only one of the parameters `G` and `K` for defining
+    the genetic background.
+
+    Let :math:`N` be the sample size, :math:`S` the number of covariates,
+    :math:`P_c` the number of genetic markers to be tested, and :math:`P_b`
+    the number of genetic markers used for Kinship estimation.
+
+    :param numpy.ndarray nsuccesses: Phenotype described by the number of
+                                     successes, as non-negative integers.
+                                     Dimension (:math:`N\\times 0`).
+    :param numpy.ndarray ntrials: Phenotype described by the number of trials,
+                                  as positive integers. Dimension
+                                  (:math:`N\\times 0`).
+    :param numpy.ndarray X: Candidate genetic markers (or any other type of
+                            explanatory variable) whose association with the
+                            phenotype will be tested. Dimension
+                            (:math:`N\\times P_c`).
+    :param numpy.ndarray G: Genetic markers matrix used internally for kinship
+                            estimation. Dimension (:math:`N\\times P_b`).
+    :param numpy.ndarray K: Kinship matrix. Dimension (:math:`N\\times N`).
+    :param numpy.array covariate: Covariates. Default is an offset.
+                                  Dimension (:math:`N\\times S`).
+    :return:              a tuple containing the estimated p-values and
+                          additional information, respectively.
+    """
+
+    logger = logging.getLogger(__name__)
+    logger.info('Association scan has started.')
+    nsuccesses = asarray(nsuccesses, dtype=float)
+
+    info = dict()
+
+    if K is not None:
+        logger.info('Covariace matrix normalization.')
+        before = time()
+        K = gower_kinship_normalization(K)
+        total = time() - before
+        print("Gower normalization: %.5f" % total)
+        info['K'] = K
+
+    if G is not None:
+        logger.info('Genetic markers normalization.')
+        before = time()
+        G = G - np.mean(G, 0)
+        s = np.std(G, 0)
+        ok = s > 0.
+        G[:, ok] /= s[ok]
+        G /= np.sqrt(G.shape[1])
+        info['G'] = G
+        total = time() - before
+        print("markers normalization: %.5f" % total)
+
+    if G is None and K is None:
+        raise Exception('G, K, and QS cannot be all None.')
+
+    logger.info('Computing the economic eigen decomposition.')
+    if K is None:
+        QS = qs_decomposition(G)
+    else:
+        QS = _QS_from_K_split(K)
+
+    logger.info('Genetic marker candidates normalization.')
+    X = X - np.mean(X, 0)
+    s = np.std(X, 0)
+    ok = s > 0.
+    X[:, ok] /= s[ok]
+    X /= np.sqrt(X.shape[1])
+    info['X'] = X
+
+    Q0, Q1 = QS[0]
+    S0 = QS[1][0]
+
+    lrt = _create_LRT(nsuccesses, Q0, Q1, S0, covariate, Binomial(ntrials),
+                      null_model_only=False)
+    lrt.candidate_markers = X
+    before = time()
+    info['lrs'] = lrt.lrs()
+    total = time() - before
+    print("lrt.lrs() : %.5f" % total)
+    info['effsizes'] = lrt.effsizes
+    return_ = (lrt.pvals(), info)
     return return_
--- limix_qep/tool/test/test_scan.py
+++ limix_qep/tool/test/test_scan.py
@@ -1,32 +1,36 @@
-# from __future__ import absolute_import
-# import unittest
-# import numpy as np
-# from numpy import dot
-#
-# from limix_qep.tool.util import create_bernoulli
-#
-# from limix_qep.tool.scan import scan
-#
+from __future__ import absolute_import
+
+import numpy as np
+from numpy import dot
+from numpy.testing import assert_array_less
+
+from limix_qep.tool.util import create_bernoulli
+
+from limix_qep.tool.scan import scan
+
+
+def test_estimate_bernoulli_real_trait():
+    np.random.seed(987)
+    nsamples = 300
+    nfeatures = 30
+    h2 = 0.9
+
+    (y, G) = create_bernoulli(nsamples, nfeatures, h2=h2, seed=894)
+    K = dot(G[:, 0:10], G[:, 0:10].T)
+
+    (pvals, _) = scan(y, G, K=K)
+
+    assert_array_less(np.sum(pvals[10:]) / 20., np.sum(pvals[:10]) / 10.)
+    assert_array_less(pvals[10:].min(), pvals[:10].min())
+    assert_array_less(pvals.min(), 1e-6)
+    assert_array_less(0.9, pvals.max())
+
 #
 # class TestScan(unittest.TestCase):
+#
 #     def setUp(self):
 #         pass
 #
-#     def test_estimate_bernoulli_real_trait(self):
-#         np.random.seed(987)
-#         nsamples = 300
-#         nfeatures = 30
-#         h2 = 0.9
-#
-#         (y, G) = create_bernoulli(nsamples, nfeatures, h2=h2, seed=894)
-#         K = dot(G[:, 0:10], G[:, 0:10].T)
-#
-#         (pvals, _) = scan(y, G, K=K)
-#
-#         self.assertTrue(np.sum(pvals[:10])/10. > np.sum(pvals[10:])/20.)
-#         self.assertTrue(pvals[:10].min() > pvals[10:].min())
-#         self.assertTrue(pvals.min() < 1e-6)
-#         self.assertTrue(pvals.max() > 0.9)
 #
 #     # def test_estimate_binomial_real_trait(self):
 #     #     seed = 3197
