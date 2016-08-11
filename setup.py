248962ab3fffd57545473aaed90bbfc25424fb5a
Danilo Horta <danilo.horta@gmail.com>
2016-08-11 10:39:59 +0100

    less restrictive test

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ from Cython.Build import cythonize
 from Cython.Distutils import build_ext
 
 PKG_NAME = 'limix_qep'
-VERSION = '0.1.11'
+VERSION = '0.1.12'
 
 try:
     from distutils.command.bdist_conda import CondaDistribution
