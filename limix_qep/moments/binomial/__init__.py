adc928d1f88877417c33eaa8a0f426814f89b0e7
Danilo Horta <danilo.horta@gmail.com>
2016-08-19 11:50:49 +0100

    moments working

--- limix_qep/moments/binomial/__init__.py
+++ limix_qep/moments/binomial/__init__.py
@@ -1,4 +1,1 @@
-from . import _binomial_ffi
-from ._binomial_ffi.lib import *
-from numba import cffi_support as _cffi_support
-_cffi_support.register_module(_binomial_ffi)
+from .binom_moments import BinomMoments
