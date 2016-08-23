358d23ce46f9400a6471c675b332abccd2863b23
Danilo Horta <danilo.horta@gmail.com>
2016-08-23 16:51:39 +0100

    dist

--- ci/test-sdist.sh
+++ ci/test-sdist.sh
@@ -7,7 +7,7 @@ pip install build_capi -q
 pip install ncephes -q
 pip install limix_math -q
 python setup.py sdist
-FILENAME=`ls sdist/ | head -1`
-pip install sdist/$FILENAME
+FILENAME=`ls dist/ | head -1`
+pip install dist/$FILENAME
 python -c "import limix_qep; limix_qep.test()"
 popd
