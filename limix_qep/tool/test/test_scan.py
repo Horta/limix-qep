e6d5b972518b1cd42352cc41ca3173b98726bd41
Danilo Horta <danilo.horta@gmail.com>
2016-08-24 09:34:36 +0100

    test_estimate_bernoulli_real_trait

--- limix_qep/tool/test/test_scan.py
+++ limix_qep/tool/test/test_scan.py
@@ -1,34 +1,33 @@
-# from __future__ import absolute_import
-#
-# import numpy as np
-# from numpy import dot
-# from numpy import full
-# from numpy.testing import assert_array_less
-#
-# from limix_qep.tool.util import create_bernoulli
-# from limix_qep.tool.dataset import create_binomial
-#
-# from limix_qep.tool.scan import scan
-# from limix_qep.tool import scan_binomial
-#
-#
-# def test_estimate_bernoulli_real_trait():
-#     np.random.seed(987)
-#     nsamples = 300
-#     nfeatures = 30
-#     h2 = 0.9
-#
-#     (y, G) = create_bernoulli(nsamples, nfeatures, h2=h2, seed=894)
-#     K = dot(G[:, 0:10], G[:, 0:10].T)
-#
-#     (pvals, _) = scan(y, G, K=K)
-#
-#     assert_array_less(np.sum(pvals[10:]) / 20., np.sum(pvals[:10]) / 10.)
-#     assert_array_less(pvals[10:].min(), pvals[:10].min())
-#     assert_array_less(pvals.min(), 1e-5)
-#     assert_array_less(0.9, pvals.max())
-#
-#
+from __future__ import absolute_import
+
+import numpy as np
+from numpy import dot
+from numpy import full
+from numpy.testing import assert_array_less
+
+from limix_qep.tool.util import create_bernoulli
+from limix_qep.tool.dataset import create_binomial
+
+from limix_qep.tool.scan import scan
+from limix_qep.tool import scan_binomial
+
+
+def test_estimate_bernoulli_real_trait():
+    np.random.seed(987)
+    nsamples = 300
+    nfeatures = 30
+    h2 = 0.9
+
+    (y, G) = create_bernoulli(nsamples, nfeatures, h2=h2, seed=894)
+    K = dot(G[:, 0:10], G[:, 0:10].T)
+
+    (pvals, _) = scan(y, G, K=K)
+
+    assert_array_less(np.sum(pvals[10:]) / 20., np.sum(pvals[:10]) / 10.)
+    assert_array_less(pvals[10:].min(), pvals[:10].min())
+    assert_array_less(pvals.min(), 1e-5)
+    assert_array_less(0.9, pvals.max())
+
 # def test_estimate_binomial_real_trait():
 #     seed = 3197
 #     nsamples = 100
