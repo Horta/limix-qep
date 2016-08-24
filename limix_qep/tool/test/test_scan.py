38741fd4704e2f141240bd07eb9ce11d8f4c3c98
Danilo Horta <danilo.horta@gmail.com>
2016-08-24 09:35:03 +0100

    binomial scan

--- limix_qep/tool/test/test_scan.py
+++ limix_qep/tool/test/test_scan.py
@@ -28,19 +28,19 @@ def test_estimate_bernoulli_real_trait():
     assert_array_less(pvals.min(), 1e-5)
     assert_array_less(0.9, pvals.max())
 
-# def test_estimate_binomial_real_trait():
-#     seed = 3197
-#     nsamples = 100
-#     nfeatures = 30
-#     ntrials = 25
-#
-#     (y, G) = create_binomial(nsamples, nfeatures, ntrials, sigg2=1.,
-#                              delta=0.01, sige2=0.01, seed=seed)
-#
-#     K = dot(G[:, 0:10], G[:, 0:10].T)
-#     zG = np.hstack((np.zeros((nsamples, 1)), G))
-#     (pvals, info) = scan_binomial(y, full(nsamples, ntrials, float), zG, K=K)
-#
-#     assert_array_less(np.sum(pvals[11:]) / 20., np.sum(pvals[1:11]) / 10.)
-#     assert_array_less(pvals[11:].min(), pvals[1:11].min())
-#     assert_array_less(pvals[1:].min(), 1e-3)
+def test_estimate_binomial_real_trait():
+    seed = 3197
+    nsamples = 100
+    nfeatures = 30
+    ntrials = 25
+
+    (y, G) = create_binomial(nsamples, nfeatures, ntrials, sigg2=1.,
+                             delta=0.01, sige2=0.01, seed=seed)
+
+    K = dot(G[:, 0:10], G[:, 0:10].T)
+    zG = np.hstack((np.zeros((nsamples, 1)), G))
+    (pvals, info) = scan_binomial(y, full(nsamples, ntrials, float), zG, K=K)
+
+    assert_array_less(np.sum(pvals[11:]) / 20., np.sum(pvals[1:11]) / 10.)
+    assert_array_less(pvals[11:].min(), pvals[1:11].min())
+    assert_array_less(pvals[1:].min(), 1e-3)
