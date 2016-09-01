19629a6a584a859b6b2f6740d2254063d38bc6dd
Danilo Horta <danilo.horta@gmail.com>
2016-09-01 11:10:27 +0100

    six.u

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ def setup_package():
     metadata = dict(
         name='limix_qep',
         maintainer="Limix Developers",
-        version='0.3.1',
+        version='0.3.2',
         maintainer_email="horta@ebi.ac.uk",
         packages=find_packages(),
         license="BSD",
