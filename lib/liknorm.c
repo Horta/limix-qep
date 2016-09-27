dd6f4f3eb7ea769b872799f5ecfe1f59d7ad3558
Danilo Horta <danilo.horta@gmail.com>
2016-09-27 16:37:44 +0100

    minimal lib

---
+++ lib/liknorm.c
@@ -0,0 +1,42 @@
+#include "liknorm/liknorm.h"
+#include <math.h>
+
+static LikNormMachine *machine;
+
+const char *liknames[] = { "binomial",    "bernoulli",    "poisson",    "gamma",
+                           "exponential", "geometric" };
+
+
+void initialize(int n)
+{
+  machine = create_liknorm_machine(350, 1e-7);
+}
+
+void destroy(void)
+{
+  destroy_liknorm_machine(machine);
+}
+
+void py_integrate(int likname_id, double *y, double *aphi,
+                  double *normal_tau, double *normal_eta, int n,
+                  double *mean, double *variance)
+{
+  Normal normal;
+  ExpFam ef;
+  log_partition *lp = get_log_partition(liknames[likname_id]);
+
+  get_interval(liknames[likname_id], &(ef.left), &(ef.right));
+
+  for (int i = 0; i < n; ++i)
+  {
+    ef.y           = y[i];
+    ef.aphi        = aphi[i];
+    ef.log_aphi    = log(ef.aphi);
+    ef.lp          = lp;
+    normal.tau     = normal_tau[i];
+    normal.eta     = normal_eta[i];
+    normal.log_tau = -log(normal.tau);
+
+    integrate(machine, &ef, &normal, mean + i, variance + i);
+  }
+}
--- lib/liknorm/README.md
+++
@@ -1,4 +0,0 @@
-# liknorm
-
-C library for computing the mean and variance of the product of an
-exponential-family likelihood with a Normal distribution.
