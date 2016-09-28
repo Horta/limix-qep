0b02c95529de8ff22c89f70cbc8ee178449ff60a
Danilo Horta <danilo.horta@gmail.com>
2016-09-28 11:52:39 +0100

    no scale

--- lib/liknorm.h
+++ lib/liknorm.h
@@ -1,5 +1,8 @@
 void initialize(int n);
 void destroy(void);
-void moments(int likname_id, double *y, double *aphi,
+void moments_scale(int likname_id, double *y, double *aphi,
                double *normal_tau, double *normal_eta, int n,
                double *log_zeroth, double *mean, double *variance);
+void moments_noscale(int likname_id, double *y,
+              double *normal_tau, double *normal_eta, int n,
+              double *log_zeroth, double *mean, double *variance);
