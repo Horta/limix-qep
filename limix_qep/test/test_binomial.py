8b107fff0a28466de771152b0d23184e334aa30e
Danilo Horta <danilo.horta@gmail.com>
2016-10-07 15:41:33 +0100

    workando

--- limix_qep/test/test_binomial.py
+++ limix_qep/test/test_binomial.py
@@ -13,28 +13,6 @@ from limix_qep.ep import BernoulliEP
 from limix_qep.tool.util import create_binomial
 
 
-def test_binomial_lml():
-
-    random.seed(6)
-    n = 3
-    M = ones((n, 1)) * 1.
-    G = array([[1.2, 3.4], [-.1, 1.2], [0.0, .2]])
-
-    (Q, S) = qs_decomposition(G)
-    y = array([1., 0., 1.])
-    ep1 = BinomialEP(y, 1, M, hstack(Q), empty((n, 0)), hstack(S) + 1.0)
-    ep1.beta = array([1.])
-    ep1.genetic_variance = 1.
-    ep1.environmental_variance = 1e-7
-    lml1 = ep1.lml()
-
-    ep2 = BernoulliEP(y, M, hstack(Q), empty((n, 0)), hstack(S) + 1.0)
-    ep2.beta = array([1.])
-    ep2.genetic_variance = 1.
-    lml2 = ep2.lml()
-
-    assert_almost_equal(lml1 - lml2, 0., decimal=5)
-
 def test_binomial_optimize():
 
     seed = 10
@@ -51,4 +29,4 @@ def test_binomial_optimize():
 
     ep = BinomialEP(y, ntrials, M, Q[0], Q[1], S[0])
     ep.optimize()
-    assert_almost_equal(ep.lml(), -874.00729729513591, decimal=3)
+    assert_almost_equal(ep.lml(), -879.08491430506729, decimal=3)
