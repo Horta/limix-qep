c67650780e8e16aea0c14e24e6dccdb23ccbb066
Danilo Horta <danilo.horta@gmail.com>
2016-10-07 13:27:30 +0100

    void poisson_moments(double size, double *k, do

--- lib/liknorm.c
+++ lib/liknorm.c
@@ -14,3 +14,12 @@ void binomial_moments(double size, double *k, double *n, double eta, double tau,
     liknorm_moments(machine, log_zeroth + i, mean + i, variance + i);
   }
 }
+
+void poisson_moments(double size, double *k, double eta, double tau,
+                     double *log_zeroth, double *mean, double *variance) {
+
+  for (size_t i = 0; i < size; i++) {
+    liknorm_set_poisson(machine, k[i]);
+    liknorm_moments(machine, log_zeroth + i, mean + i, variance + i);
+  }
+}
--- lib/liknorm.h
+++ lib/liknorm.h
@@ -2,3 +2,5 @@ void initialize(int n);
 void destroy(void);
 void binomial_moments(double size, double *k, double *n, double eta, double tau,
                       double *log_zeroth, double *mean, double *variance);
+void poisson_moments(double size, double *k, double eta, double tau,
+                     double *log_zeroth, double *mean, double *variance);
