5ef705298f7a047c727a5a19c93f7e2bb7adbf12
Danilo Horta <danilo.horta@gmail.com>
2016-10-10 16:11:45 +0100

    binomial is working

--- limix_qep/test/test_binomial.py
+++ limix_qep/test/test_binomial.py
@@ -1,9 +1,13 @@
+from __future__ import division
+
 from numpy import ones
+from numpy import sqrt
 from numpy import array
+from numpy import asarray
 from numpy import empty
 from numpy import hstack
-from numpy import random
 from numpy.testing import assert_almost_equal
+from numpy.random import RandomState
 
 from limix_math.linalg import qs_decomposition
 
@@ -12,21 +16,45 @@ from limix_qep.ep import BernoulliEP
 
 from limix_qep.tool.util import create_binomial
 
+import lim
+
 
 def test_binomial_optimize():
 
-    seed = 10
+    random = RandomState(10)
     nsamples = 200
     nfeatures = 1200
-    ntrials = random.RandomState(seed).randint(1, 1000, nsamples)
+    ntrials = random.randint(1, nsamples, nsamples)
 
-    M = ones((nsamples, 1))
+    G = random.randn(nsamples, nfeatures)
+    G = (G - G.mean(0)) / G.std(0)
+    G /= sqrt(200)
 
-    (y, G) = create_binomial(nsamples, nfeatures, ntrials, var=1.0,
-                             delta=0.1, sige2=0.1, seed=seed)
+    mean = lim.mean.OffsetMean()
+    mean.offset = 0.0
+    mean.set_data(nsamples, 'sample')
+
+    cov1 = lim.cov.LinearCov()
+    cov1.set_data((G, G), 'sample')
+
+    cov2 = lim.cov.EyeCov()
+    a = lim.util.fruits.Apples(nsamples)
+    cov2.set_data((a, a), 'sample')
+
+    cov1.scale = 1.0
+    cov2.scale = 1.0
+
+    cov = lim.cov.SumCov([cov1, cov2])
+
+    link = lim.link.LogitLink()
+    lik = lim.lik.Binomial(0, ntrials, link)
+    nsuc = lim.random.GLMMSampler(lik, mean, cov).sample(random)
 
     (Q, S) = qs_decomposition(G)
 
-    ep = BinomialEP(y, ntrials, M, Q[0], Q[1], S[0])
+    ep = BinomialEP(nsuc, ntrials, ones((nsamples, 1)), Q[0], Q[1], S[0])
     ep.optimize()
-    assert_almost_equal(ep.lml(), -879.08491430506729, decimal=3)
+    assert_almost_equal(ep.lml(), -827.230261501, decimal=4)
+
+if __name__ == '__main__':
+    __import__('pytest').main([__file__, '-s'])
